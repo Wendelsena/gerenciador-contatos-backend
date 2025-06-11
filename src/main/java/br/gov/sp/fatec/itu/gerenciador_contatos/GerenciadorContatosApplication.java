efdbc1507099a09d364110596fd36fa1ae63bedb
Wendel Sena <wendel4019@gmail.com>
2025-06-11 13:07:22 -0300

    Projeto inicial

---
+++ src/main/java/br/gov/sp/fatec/itu/gerenciador_contatos/GerenciadorContatosApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.itu.gerenciador_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/itu/gerenciador_contatos/GerenciadorContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.itu.gerenciador_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
